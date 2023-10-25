817c8da6a13c56ab8fd62148c4b24f56d14a20d7
m1bore <122971250+m1bore@users.noreply.github.com>
2023-10-25 12:42:01 +0200

    Create ec2.sh

---
+++ ec2.sh
@@ -0,0 +1,17 @@
+AWS_ID_GrupoSeguridad_EC2MILES=$(aws ec2 create-security-group \ 
+	--group-name 'grupoEC2MILES' \
+	--description 'grupo para habilitar ssh' \
+	--output text)
+
+aws ec2 authorize-security-group-ingress \
+	--group-id $AWS_ID_GrupoSeguridad_EC2MILES \
+	--ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "ipRanges": [{"CidrIp": "0.0.0.0/0", "Description": "Allow SSH"}]}]'
+
+aws ec2 run-instances \
+--image-id ami-050406429a71aaa64 \
+--count 1 \
+--instance-type m1.small \
+--key-name vockey \
+--region us-east-1 \
+--security-group-ids $AWS_ID_GrupoSeguridad_EC2MILES \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=EC2MILES}]'
